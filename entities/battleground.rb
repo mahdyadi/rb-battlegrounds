c29b6c3e8b07c27811a0136a5c9003205fdbc016
Drianka Mahdy Adimas <drianka.mahdy@gmail.com>
2020-02-20 11:59:47 +0700

    Finished battleground definition for now

--- entities/battleground.rb
+++ entities/battleground.rb
@@ -1,5 +1,50 @@
 class Battleground
     def initialize(columns,rows)
-        @field = Hash.new(:empty)
+        raise ArgumentError,"columns and rows passed needs to be numeric" unless (columns.is_a?(Numeric) && rows.is_a?(Numeric))
+
+        @tiles = Array.new(columns,Array.new(rows,:empty))
+    end
+
+    def get_object(columns,row)
+        obj_at_position = @tiles[columns][row]
+        return nil unless obj_at_position != :empty
+        return obj_at_position
+    end
+
+    def place_object(obj,position)
+        raise "passed object/first argument doesn't have orientation" unless obj.respond_to?(:orientation)
+        raise "passed object/first argument doesn't have length" unless obj.respond_to?(:orientation)
+
+        raise "passed object/first argument's length should be a number" unless obj.length.is_a?(Numeric)
+
+        raise "position/second argument should be a two-length array" unless (position.is_a?(Array) && position.length == 2)
+
+        raise "all element in the position array should be numeric" unless (position.all?{|elem| elem.is_a?(Numeric)})
+
+        raise "passed position should be in the range of current tiles" unless (
+            @tiles.length > position[0] && 
+            position[0] >= 0 && 
+            @tiles[0].length > position[1] &&
+            position[1] >= 0
+        )
+
+        raise "passed object length cannot be zero" unless obj.length > 0
+
+        case obj.orientation
+        when :vertical
+            raise "cannot find available space for the object #{obj} in the battleground" unless @tiles.length > (position[0]+obj.length-1)
+
+            (0...obj.length).each do |idx|
+                @tiles[position[0]+idx][position[1]] = obj
+            end
+        when :horizontal
+            raise "cannot find available space for the object #{obj} in the battleground" unless @tiles[0].length > (position[1]+obj.length-1)
+
+            (0...obj.length).each do |idx|
+                @tiles[position[0]][position[1]+idx] = obj
+            end
+        else
+            raise "passed object's orientation needs to return a :vertical or :horizontal"
+        end
     end
 end
--- spec/battleground_spec.rb
+++ spec/battleground_spec.rb
@@ -1,7 +1,39 @@
 require_relative "../entities/battleground.rb"
 
+class TestObject
+    def orientation
+        :vertical
+    end
+
+    def length
+        1
+    end
+
+    def to_s
+        "hehehe"
+    end
+end
+
 RSpec.describe "Battleground" do
     it "can be built by giving number of rows and columns" do
         battleground = Battleground.new(1,1)
+    end
+
+    it "should raise error if initialized with non number arguments" do
+        expect{Battleground.new("foo",:bar)}.to raise_error(ArgumentError)
+    end
+
+    it "get_object returns nil if a tile coordinate is given but no object/part of exist in that tile" do
+        battleground = Battleground.new(1,1)
+        result = battleground.get_object(0,0)
+        expect(result).to eql(nil)
+    end
+
+    it "get_object returns the object if a given tile coordinate is the location of an object/part of an object" do
+        battleground = Battleground.new(5,5)
+        test_obj = TestObject.new
+        battleground.place_object(test_obj,[1,2])
+        result = battleground.get_object(1,2)
+        expect(result).to eql(test_obj)
     end
 end
