5d1095f39332122143bec0cf83cd9b43806fd824
Drianka Mahdy Adimas <drianka.mahdy@gmail.com>
2020-02-20 11:52:42 +0700

    Restructure the project & test files

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
+gem 'bundler', '~>2.1.4'
 gem "rspec", "~> 3.9"
--- spec/battleground_spec.rb
+++ spec/battleground_spec.rb
@@ -1,6 +1,6 @@
-require_relative "../entities/battleground.rb"
+require_relative "../lib/entities/battleground.rb"
 
-class TestObject
+class MockBattlegroundCompliantObject
     def orientation
         :vertical
     end
@@ -14,26 +14,90 @@ class TestObject
     end
 end
 
-RSpec.describe "Battleground" do
-    it "can be built by giving number of rows and columns" do
-        battleground = Battleground.new(1,1)
+class MockBattlegroundIncompliantObject
+end
+
+RSpec.describe Battleground do
+    describe ".initialize" do
+        context "when called by passing vector containing column & row number as args" do
+            it "should succesfully creates new instance" do
+                battleground = Battleground.new([1,1])
+            end
+
+            it "should raise error if given non number value" do
+                expect {Battleground.new(["foo",1])}.to raise_error(ArgumentError)
+                expect {Battleground.new([1,"foo"])}.to raise_error(ArgumentError)
+            end
+
+            it "should raise error if given zero column/row number" do
+                expect {Battleground.new([0,1])}.to raise_error(ArgumentError)
+                expect {Battleground.new([1,0])}.to raise_error(ArgumentError)
+            end
+        end
+
+        context "when called by passing vector containing column and row and also initial object" do
+            it "should successfully creates new instance" do
+                obj = MockBattlegroundCompliantObject.new
+                battleground = Battleground.new([1,1],[obj,[0,0]])
+            end
+
+            it "should raise error if given non number value for the position" do
+                obj = MockBattlegroundCompliantObject.new
+                expect {Battleground.new([0,0],[obj,["foo",1]])}.to raise_error(ArgumentError)
+                expect {Battleground.new([0,0],[obj,[1,"foo"]])}.to raise_error(ArgumentError)
+            end
+
+            it "should raise error if given initial object which does not conform to the format of [object, [column position, row position]]" do
+                obj = MockBattlegroundCompliantObject.new
+                expect {Battleground.new([5,5],[obj,1,2])}.to raise_error(ArgumentError)
+                expect {Battleground.new([5,5],[obj,[1]])}.to raise_error(ArgumentError)
+            end
+        end
     end
 
-    it "should raise error if initialized with non number arguments" do
-        expect{Battleground.new("foo",:bar)}.to raise_error(ArgumentError)
+
+    describe "#get_object" do
+        context "when given position as arguments" do
+            it "should returns nil if no object/part of it exist in specified position" do
+                battleground = Battleground.new([1,1])
+                result = battleground.get_object(0,0)
+                expect(result).to eql(nil)
+            end
+
+            it "should returns the object if the location matches" do
+                test_obj = MockBattlegroundCompliantObject.new
+                battleground = Battleground.new([5,5],[test_obj,[0,0]])
+                result = battleground.get_object(0,0)
+                expect(result).to eql(test_obj)
+            end
+
+            it "should raise error if position is outside range of the battlegrounds" do
+                battleground = Battleground.new([5,5])
+                expect {battleground.get_object(5,5)}.to raise_error(ArgumentError)
+            end
+        end
+
+        context "when given no arguments" do
+            it "should raise error" do
+                battleground = Battleground.new([1,1])
+                expect {battleground.get_object()}.to raise_error(ArgumentError)
+            end
+        end
     end
 
-    it "get_object returns nil if a tile coordinate is given but no object/part of exist in that tile" do
-        battleground = Battleground.new(1,1)
-        result = battleground.get_object(0,0)
-        expect(result).to eql(nil)
-    end
+    describe "#place_object" do
+        context "When given a vector of object and position" do
+            it "should return nil and successfully place object" do
+                correct_obj = MockBattlegroundCompliantObject.new
+                battleground = Battleground.new([1,1])
+                expect {battleground.place_object(correct_obj,[0,0])}.not_to raise_error
+            end
 
-    it "get_object returns the object if a given tile coordinate is the location of an object/part of an object" do
-        battleground = Battleground.new(5,5)
-        test_obj = TestObject.new
-        battleground.place_object(test_obj,[1,2])
-        result = battleground.get_object(1,2)
-        expect(result).to eql(test_obj)
+            it "should raise error when the object is not conforming to the required interface" do
+                incorrect_obj = MockBattlegroundIncompliantObject.new
+                battleground = Battleground.new([1,1])
+                expect {battleground.place_object(incorrect_obj,[0,0])}.to raise_error(ArgumentError)
+            end
+        end
     end
 end
