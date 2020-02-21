a4b9e0e9f48a094b0f3fc94ad366ecc98f22f295
Drianka Mahdy Adimas <drianka.mahdy@gmail.com>
2020-02-21 10:56:58 +0700

    Add new test for player initialization

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,17 @@
 require_relative "../lib/entities/player.rb"
 
-RSpec.describe "Player" do
-    it "is able to be initialized" do
+class MockField
+    def initialize
+    end
+end
+
+RSpec.describe Player do
+    describe ".initialize" do
+        context "When passed with field-compliant object" do
+            it "Could be executed successfully" do
+                field_compliant_obj = MockField.new
+                expect{player_in_test = Player.new(field_compliant_obj)}.not_to raise_error
+            end
+        end
     end
 end
