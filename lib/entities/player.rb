0b04d11291f7a72e2fa93994905366e443f2b932
Drianka Mahdy Adimas <drianka.mahdy@gmail.com>
2020-02-22 17:23:12 +0700

    Add basic implementation of ship getting damage

--- lib/entities/player.rb
+++ lib/entities/player.rb
@@ -7,18 +7,20 @@ class Player
     raise ArgumentError, "passed field object should have #place_object method" unless field.respond_to?(:place_object)
 
     unless field.respond_to?(:get_object)
-        raise "passed field object should have defined #get_object method"
+      raise "passed field object should have defined #get_object method"
     end
 
     @field = field
     @ammo = ammunition
   end
 
-  def receive_attack((x,y))
-    ship = @field.get_object([x,y])
+  def receive_attack((x, y))
+    ship = @field.get_object([x, y])
     unless ship != nil
-        return -1
+      return -1
     end
+
+    return 0
   end
 
   def place_ship(ship, (x, y))
